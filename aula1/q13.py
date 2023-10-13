028c7e907b382dfa1be5d33932e4a92e0ae1688f
Jadersonfreitas <146891720+Jadersonfreitas@users.noreply.github.com>
2023-10-13 14:51:47 -0300

    q13.py

--- aula1/q13.py
+++ aula1/q13.py
@@ -1,4 +1,5 @@
-n1 = bool(input("insira o primeiro numero:"))
-n2 = bool(input("insira o segundo numero:"))
+n1 = float(input("digite um numero:"))
+n2 = float(input("digite outro numero:"))
 
-print("os dois numeros são iguais:", (n1 == n2), "!")
+print(f"""o primeiro numero é igual ao segundo?
+      {n1 == n2}""")
